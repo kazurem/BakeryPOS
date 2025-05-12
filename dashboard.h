8e94a937dbcf8c635938d6a85ae4d78ae70cba1a
sora <123005041+kazurem@users.noreply.github.com>
2025-05-12 00:00:17 -0700

    Setup the project

---
+++ dashboard.h
@@ -0,0 +1,23 @@
+#ifndef DASHBOARD_H
+#define DASHBOARD_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class dashboard;
+}
+QT_END_NAMESPACE
+
+class dashboard : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    dashboard(QWidget *parent = nullptr);
+    ~dashboard();
+
+private:
+    Ui::dashboard *ui;
+};
+#endif // DASHBOARD_H
